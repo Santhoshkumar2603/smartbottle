fdc74f0965a9824be394bec2578a24b9dab78d2c
Santhoshkumar2603 <sant18ec116@rmkcet.ac.in>
2020-11-05 23:53:00 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -12,7 +12,27 @@ var firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   
 
-  
+  const inputs = document.querySelectorAll(".input");
+
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
 
   $("#btn-login").click(function()
   {
@@ -109,6 +129,7 @@ var firebaseConfig = {
 
   $("#btn-logout").click(function()
   {
+       window.location.href ="signin.html";
        firebase.auth().signOut();
   });
    
